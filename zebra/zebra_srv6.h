309dc0b81c19017660e04f9dd1232dd1ce8bef58
Hiroki Shirokura <slank.dev@gmail.com>
2019-11-20 08:38:16 +0000

    WIP sid master entities

--- zebra/zebra_srv6.h
+++ zebra/zebra_srv6.h
@@ -40,6 +40,8 @@ struct seg6local_sid {
 	uint32_t plen;
 
 	uint32_t action; /* SEG6_LOCAL_ACTION_{HOGE, END, END_X} */
+
+	uint32_t master; /* daemon-id bgpd,isisd,etc... */
 };
 
 #define MAX_SEG6LOCAL_SIDS 1024
