3c6b31df841450307851c2f01d321573a3d431c8
Hiroki Shirokura <slank.dev@gmail.com>
2020-09-30 12:59:19 +0900

    *: new cli-nodes for SRv6 manager (step2)
    This commit is a part of #5853 that add new cmd-node for SRv6 configuration.
    This commit just add cmd-node and moving node cli only, acutual SRv6 config
    command isn't added. (that is added later commit. of this branch)
    
    new cli nodes:
    * SRv6
    * SRv6-locators
    * SRv6-locator
    
    Signed-off-by: Hiroki Shirokura <slank.dev@gmail.com>

--- zebra/main.c
+++ zebra/main.c
@@ -58,6 +58,7 @@
 #include "zebra/zebra_nb.h"
 #include "zebra/zebra_opaque.h"
 #include "zebra/zebra_srte.h"
+#include "zebra/zebra_srv6_vty.h"
 
 #define ZEBRA_PTM_SUPPORT
 
@@ -417,6 +418,7 @@ int main(int argc, char **argv)
 	zebra_pbr_init();
 	zebra_opaque_init();
 	zebra_srte_init();
+	zebra_srv6_vty_init();
 
 	/* For debug purpose. */
 	/* SET_FLAG (zebra_debug_event, ZEBRA_DEBUG_EVENT); */
