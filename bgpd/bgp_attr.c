2848a6438de671d437ed0b151b53ab64b583d8e9
Hiroki Shirokura <slank.dev@gmail.com>
2020-12-24 07:22:49 +0000

    fix ?

--- bgpd/bgp_attr.c
+++ bgpd/bgp_attr.c
@@ -676,6 +676,10 @@ unsigned int attrhash_key_make(const void *p)
 		MIX(transit_hash_key_make(bgp_attr_get_transit(attr)));
 	if (attr->encap_subtlvs)
 		MIX(encap_hash_key_make(attr->encap_subtlvs));
+	if (attr->srv6_l3vpn)
+		MIX(srv6_l3vpn_hash_key_make(attr->srv6_l3vpn));
+	if (attr->srv6_vpn)
+		MIX(srv6_vpn_hash_key_make(attr->srv6_vpn));
 #ifdef ENABLE_BGP_VNC
 	struct bgp_attr_encap_subtlv *vnc_subtlvs =
 		bgp_attr_get_vnc_subtlvs(attr);
@@ -1141,6 +1145,16 @@ void bgp_attr_undup(struct attr *new, struct attr *old)
 
 	if (new->lcommunity != old->lcommunity)
 		lcommunity_free(&new->lcommunity);
+
+	if (new->srv6_l3vpn != old->srv6_l3vpn) {
+		srv6_l3vpn_free(new->srv6_l3vpn);
+		new->srv6_l3vpn = NULL;
+	}
+
+	if (new->srv6_vpn != old->srv6_vpn) {
+		srv6_vpn_free(new->srv6_vpn);
+		new->srv6_vpn = NULL;
+	}
 }
 
 /* Free bgp attribute and aspath. */
@@ -1201,6 +1215,14 @@ void bgp_attr_flush(struct attr *attr)
 	if (attr->encap_subtlvs && !attr->encap_subtlvs->refcnt) {
 		encap_free(attr->encap_subtlvs);
 		attr->encap_subtlvs = NULL;
+	}
+	if (attr->srv6_l3vpn && !attr->srv6_l3vpn->refcnt) {
+		srv6_l3vpn_free(attr->srv6_l3vpn);
+		attr->srv6_l3vpn = NULL;
+	}
+	if (attr->srv6_vpn && !attr->srv6_vpn->refcnt) {
+		srv6_vpn_free(attr->srv6_vpn);
+		attr->srv6_vpn = NULL;
 	}
 #ifdef ENABLE_BGP_VNC
 	struct bgp_attr_encap_subtlv *vnc_subtlvs =
